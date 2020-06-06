178bb2bd1a897d1d3cb736ee63cd628644ef30e2
Bernard Laird <bernard.laird@paddypowerbetfair.com>
2020-06-06 03:40:47 +0100

    Add project.
    Add gitignore file
    Add health endpoint
    Add swagger + swagger UI
    Add Docker build file

---
+++ src/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CovidStatApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
